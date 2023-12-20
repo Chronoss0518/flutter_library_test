05eef678c051cfab9917ab7cb4049ba275d0c575
Chronoss0518 <sinyusuke5000@gmail.com>
2023-12-21 02:15:04 +0900

    Redmeを更新

--- lib/main.dart
+++ lib/main.dart
@@ -51,16 +51,21 @@ class StartScene extends BaseScene {
 
   @override
   Widget build(BuildContext context) {
+    MediaQuery.of(context).size;
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          const Text(
+          DrawText(
             'You have pushed the button this many times:',
           ),
-          Text(
-            'Test:' + testNumber.toString(),
-            style: Theme.of(context).textTheme.headlineMedium,
+          Container(
+            margin: EdgeInsets.only(left: 100.0),
+            color: Colors.red,
+            child: Text(
+              'Test:' + testNumber.toString(),
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
           ),
         ],
       ),
@@ -77,6 +82,16 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class DrawText extends StatelessWidget {
+  DrawText(this.text, {super.key});
+
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Text(text);
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
@@ -88,39 +103,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -137,7 +126,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
